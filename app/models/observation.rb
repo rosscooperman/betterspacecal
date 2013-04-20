2d5821392d05cc498fd7ac024488c760e64a2f05
Ross Cooperman <cooperman@gmail.com>
2013-04-20 15:05:49 -0400

    example of using all known attributes of observation objects

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,7 +1,7 @@
 class Observation
   include HTTParty
 
-  attr_accessor :target
+  attr_accessor :source, :target, :ra, :dec, :ra_str, :dec_str, :b, :l, :end, :start
 
   def initialize(attributes = {})
     attributes.each do |k, v|
@@ -18,5 +18,13 @@ class Observation
     observations.map do |observation|
       new(observation)
     end
+  end
+
+  def end=(val)
+    @end = val.is_a?(String) ? Time.zone.parse(val) : val
+  end
+
+  def start=(val)
+    @start = val.is_a?(String) ? Time.zone.parse(val) : val
   end
 end
