eda099387275cfc98644d971efd97d73aa0ae436
savagecorp <savagecorp@gmail.com>
2013-04-20 15:58:06 -0400

    style updates

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,9 @@
+$(window).load(function() {
+  
+  // hardcoded size for background image
+  $('.skymap').css('height','300px' );
+  
+  // update so on the height of the div is half of the width width
+  
+  
+});
