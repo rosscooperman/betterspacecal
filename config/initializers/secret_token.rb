e4d17a18aad9c542bde4bcba3d59841ed9215894
Ross Cooperman <cooperman@gmail.com>
2013-04-20 12:56:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Betterspacecal::Application.config.secret_token = 'e66e7f91f0823071cdd5e2d43f72d09e1cefad1bf71d9682fbe04f704950e3a0decfe35c944d0a9c9805ae039964e357b1001fa768dfb347b39c64a5ac5df162'
