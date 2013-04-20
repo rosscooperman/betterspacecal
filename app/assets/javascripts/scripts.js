f1a9d605b8621bf7357825a970380750ea70ede8
Ross Cooperman <cooperman@gmail.com>
2013-04-20 16:28:28 -0400

    resize the skymap div when the browser is resized (and when the page loads)

--- app/assets/javascripts/scripts.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function() {
-  
-  // hardcoded size for background image
-  $('.skymap').css('height','300px' );
-  
-  // update so on the height of the div is half of the width width
-  
-  
-});
