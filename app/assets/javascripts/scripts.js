90e7c241603fe0d8184a3b813113e1c216cca47e
savagecorp <savagecorp@gmail.com>
2013-04-20 22:41:27 -0400

    fix dropbown bug for check boxes

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,8 +1,10 @@
-$(".hotdrop").hover(
-  function () {
-    $(".opticalDrop").slideToggle(300)
-  },
-  function () {
-    $(".opticalDrop").slideToggle(100)
-  }
-);
+$(document).ready(function(){
+  $(".hotdrop").hover(
+    function () {
+      $(".opticalDrop").slideToggle(300)
+    },
+    function () {
+      $(".opticalDrop").slideToggle(100)
+    }
+  );
+});
