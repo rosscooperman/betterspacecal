94f2c951ab20e62acae80c2fcd36550437f7add7
jhn <johan.menac@gmail.com>
2013-04-22 19:48:57 -0400

    Add 'Back to Top' button

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
-  
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 600) {
+      $('.go-top').fadeIn(200);
+    } else {
+      $('.go-top').fadeOut(200);
+    }
+  });
+
+  $('.go-top').click(function(e) {
+    e.preventDefault();
+    $('body').animate({scrollTop: 0}, 300);
+  });
+
   // show/hide filter fields
   $('#filterdata').click(function(){
     $('.filters').slideToggle();
   });
-  
+
   // dropdown functionality on the filters
   $(".hotdrop").hover(
     function () {
@@ -15,7 +28,4 @@ $(document).ready(function(){
     }
   );
   var viewportHeight = $(window).height();
-  
-  
-  
 });
