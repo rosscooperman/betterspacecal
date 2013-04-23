d7bad891f049166730e7b37b3d472f699c6d61c2
Sushant <mailsushant@gmail.com>
2013-04-23 00:41:07 -0400

    Lookup SIMBAD database for target images not found on NED.

--- backend/image-grabber.py
+++ backend/image-grabber.py
@@ -3,11 +3,15 @@ import urllib
 import pymongo
 import requests
 
-from PIL import Image
 from BeautifulSoup import BeautifulSoup
 
-DOMAIN = 'http://ned.ipac.caltech.edu'
-BASE_URL = DOMAIN + '/cgi-bin/imgdata?objname='
+NED_DOMAIN = 'http://ned.ipac.caltech.edu'
+NED_BASE_URL = NED_DOMAIN + '/cgi-bin/imgdata?objname='
+
+SIMBAD_DOMAIN = 'http://simbad.u-strasbg.fr'
+SIMBAD_BASE_URL = SIMBAD_DOMAIN + '/simbad/sim-id?Ident='
+SIMBAD_IMAGE_URL_FORMAT = 'http://alasky.u-strasbg.fr/cgi/simbad-thumbnails/get-thumbnail.py?oid=%d&size=200&legend=false'
+
 
 def run():
   targets = get_targets()
@@ -44,24 +48,55 @@ def get_targets():
 
 
 def get_images(target):
+  images = get_images_from_NED(target)
+  if not images:
+    images = get_images_from_SIMBAD(target)
+
+  if images:
+    return {'_id': target, 'images': images}
+  else:
+    print 'No images found for ' + target
+    return []
+
+
+def get_images_from_NED(target):
   images = []
   try:
-    url = BASE_URL + urllib.quote(target)
+    url = NED_BASE_URL + urllib.quote(target)
     html = requests.get(url).text
     soup = BeautifulSoup(html)
     rows = soup.findAll('tr')
     rows = rows[1:]
     for row in rows:
-      image_url = DOMAIN + row.find('td').find('a')['href']
+      image_url = NED_DOMAIN + row.find('td').find('a')['href']
       images.append(image_url)
-    if images:
-      return {'_id': target, 'images': images}
-    else:
-      print 'No images for ' + target
-      return None
+    return images
   except Exception as e:
-    print time.asctime() + ' | ERROR | Failed to get images for ' + target + '\n' + str(e)
-    return None
+    print time.asctime() + ' | ERROR | Failed to get NED images for ' + target + '\n' + str(e)
+    return []
+
+
+def get_images_from_SIMBAD(target):
+  """
+      Sample image URL on SIMBAD:
+      http://alasky.u-strasbg.fr/cgi/simbad-thumbnails/get-thumbnail.py?oid=3214151&
+      size=200&legend=false&reticle=true&reticleWidth=1&reticleColor=yellow&scale=true
+  """
+  images = []
+  try:
+    url = SIMBAD_BASE_URL + urllib.quote(target)
+    html = requests.get(url).text
+    soup = BeautifulSoup(html)
+    image_tags = soup.findAll('img')
+    for image in image_tags:
+      if 'oid' in image['src']:  # if oid in img src, there is an image for this target
+        image_url = image['src']
+        images.append(image_url)
+    return images
+  except Exception as e:
+    import traceback
+    print time.asctime() + ' | ERROR | Failed to get SIMBAD images for ' + target + '\n' + traceback.format_exc()
+    return []
 
 
 if __name__ == '__main__':
